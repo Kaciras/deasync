d7b4010f8090d6ac33afa9528d05fae527f1338d
David Baldwynn <whitef0x0@users.noreply.github.com>
2015-09-03 10:07:35 -0700

    Added Math.random() and comment that explains what it does

--- index.js
+++ index.js
@@ -1,59 +1,71 @@
-var fs = require('fs'),
-	path = require('path');
-var binding;
-
-// Seed random numbers [gh-82]
-Math.random();
-
-// Look for binary for this platform
-var nodeV = 'node-' + /[0-9]+\.[0-9]+/.exec(process.versions.node)[0];
-var modPath = path.join(__dirname, 'bin', process.platform + '-' + process.arch + '-' + nodeV, 'deasync');
-try {
-	fs.statSync(modPath + '.node');
-	binding = require(modPath);
-}
-catch (ex) {
-	binding = require('bindings')('deasync');
-}
-
-
-
-function deasync(fn) {
-	return function() {
-		var done = false;
-		var args = Array.prototype.slice.apply(arguments).concat(cb);
-		var err;
-		var res;
-
-		fn.apply(this, args);
-		module.exports.loopWhile(function(){return !done;});
-		if (err)
-			throw err;
-
-		return res;
-
-		function cb(e, r) {
-			err = e;
-			res = r;
-			done = true;
+/*!
+ * deasync
+ * https://github.com/abbr/deasync
+ *
+ * Copyright 2010-2015 Abbr
+ * Released under the MIT license
+ */
+ 
+(function () {
+		
+	var fs = require('fs'),
+		path = require('path'),
+		binding;
+	
+	// Seed random numbers [gh-82]
+	Math.random();
+	
+	// Look for binary for this platform
+	var nodeV = 'node-' + /[0-9]+\.[0-9]+/.exec(process.versions.node)[0];
+	var modPath = path.join(__dirname, 'bin', process.platform + '-' + process.arch + '-' + nodeV, 'deasync');
+	try {
+		fs.statSync(modPath + '.node');
+		binding = require(modPath);
+	}
+	catch (ex) {
+		binding = require('bindings')('deasync');
+	}
+	
+	
+	
+	deasync = function (fn) {
+		return function() {
+			var done = false,
+				err,
+				res;
+			var cb = function (e, r) {
+				err = e;
+				res = r;
+				done = true;
+			}
+			var args = Array.prototype.slice.apply(arguments).concat(cb);
+	
+			fn.apply(this, args);
+			
+			//Wait until cb() has been called
+			module.exports.loopUntil(function(){return !done;});
+			
+			if (err) throw err;
+			return res;
 		}
 	}
-}
+	
+	module.exports = deasync;
+	
+	module.exports.sleep = deasync(function(timeout, done) {
+		setTimeout(done, timeout);
+	});
+	
+	module.exports.runLoopOnce = function(){
+		process._tickDomainCallback();
+		binding.run();
+	};
+	
+	module.exports.loopUntil = function(pred){
+	  while(pred()){
+		process._tickDomainCallback();
+		if(pred()) binding.run();
+	  }
+	};
 
-module.exports = deasync;
-
-module.exports.sleep = deasync(function(timeout, done) {
-	setTimeout(done, timeout);
-});
-
-module.exports.runLoopOnce = function(){
-	process._tickDomainCallback();
-	binding.run();
-};
-
-module.exports.loopUntil = function(pred){
-  while(pred()){
-	process._tickDomainCallback();
-	if(pred()) binding.run();
-  }
-};
+}());
