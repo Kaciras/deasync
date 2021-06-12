1a8b98b4894bb6f8e86dcc70da826515d3cce7f0
Kaciras <Kaciras@protonmail.com>
2021-06-12 12:29:27 +0800

    add build script & CI

---
+++ script/prebuild.js
@@ -0,0 +1,101 @@
+const { execSync } = require("child_process");
+const { join } = require("path");
+const fs = require("fs");
+const { createBrotliCompress, createBrotliDecompress } = require("zlib");
+const { https } = require("follow-redirects");
+const tar = require("tar-fs");
+const packageJson = require("../package.json");
+
+process.chdir(join(__dirname, ".."));
+
+/**
+ * Get the tarball name of current environment.
+ */
+function getPackageName() {
+	const name = packageJson.name.split("/").pop();
+	const { version } = packageJson;
+	const runtime = "node";
+	const abi = "NAPIv3";
+	const { platform, arch } = process;
+
+	return `${name}-v${version}-${runtime}-v${abi}-${platform}-${arch}.tar.br`;
+}
+
+function getGithubRelease() {
+	const re = new RegExp('github.com/([^/"]+)/([^/"]+)');
+	const { version, repository } = packageJson;
+
+	const match = re.exec(JSON.stringify(repository));
+	if (!match) {
+		throw new Error("You must set the repository property in package.json");
+	}
+
+	return `https://${match[0]}/releases/download/v${version}`.replace(/\.git$/, "");
+}
+
+/**
+ * Pack the prebuilt binary file and compress it with brotli.
+ */
+function pack() {
+	fs.rmSync("prebuilds", { recursive: true, force: true });
+	fs.mkdirSync("prebuilds");
+
+	const pack = tar.pack(".", {
+		entries: ["build/Release/binding.node"],
+	});
+
+	const file = `prebuilds/${getPackageName()}`;
+	console.log(`Packing files to ${file}`);
+
+	pack.pipe(createBrotliCompress()).pipe(fs.createWriteStream(file));
+}
+
+/**
+ * Download prebuilt binary from GitHub Release.
+ * If download failed, fallback to build locally.
+ */
+function download() {
+	const url = `${getGithubRelease()}/${getPackageName()}`;
+	const request = https.get(url);
+
+	request.on("response", response => {
+		const { statusCode } = response;
+
+		if (statusCode === 404) {
+			return handleInstallError(`No prebuilt binary for your environment：${url}`);
+		} else if (statusCode !== 200) {
+			return handleInstallError(`Download failed (${statusCode}): ${url}`);
+		}
+
+		response
+			.pipe(createBrotliDecompress())
+			.pipe(tar.extract("."));
+	});
+
+	request.on("error", handleInstallError).end();
+}
+
+function handleInstallError(error) {
+	console.warn(error);
+
+	try {
+		const cmd = "node-gyp rebuild --ensure";
+		execSync(cmd, { stdio: "inherit" });
+	} catch (e) {
+		process.exit(e.status);
+	}
+}
+
+const { NO_PREBUILD, CI } = process.env;
+const [, , verb] = process.argv;
+
+if (verb === "install") {
+	if (!NO_PREBUILD && !CI) {
+		download();
+	}
+} else if (verb === "pack") {
+	pack();
+} else {
+	console.error("Parameter required: install or pack");
+	process.exit(1);
+}
