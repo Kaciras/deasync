bf6ce6ae17c8644679b195823cb5e5cdb9e64acd
Kaciras <Kaciras@protonmail.com>
2021-06-24 13:14:09 +0800

    Add partial request support

--- script/prebuild.js
+++ script/prebuild.js
@@ -22,15 +22,15 @@ function getPackageName() {
 }
 
 function getGithubRelease() {
-	const re = new RegExp('github.com/([^/"]+)/([^/"]+)');
+	const re = new RegExp('github.com/[^/"]+/[^/"]+');
 	const { version, repository } = packageJson;
 
 	const match = re.exec(JSON.stringify(repository));
 	if (!match) {
 		throw new Error("You must set the repository property in package.json");
 	}
-
-	return `https://${match[0]}/releases/download/v${version}`.replace(/\.git$/, "");
+	const url = match[0].replace(/\.git$/, "");
+	return `https://${url}/releases/download/v${version}`;
 }
 
 /**
@@ -50,26 +50,75 @@ function pack() {
 	pack.pipe(createBrotliCompress()).pipe(fs.createWriteStream(file));
 }
 
+function checkRange(request, response) {
+	const reqHeader = request.getHeader("Range");
+	const resHeader = response.headers["content-range"];
+	const match1 = /bytes=(\d*)-(\d*)/.exec(reqHeader);
+	const match2 = /bytes (\d+)-(\d+)\/(\d+)/.exec(resHeader);
+
+	if (!match1 || !match2) {
+		return false;
+	}
+	const offset = parseInt(match2[1]);
+	const end = parseInt(match2[2]);
+	const total = parseInt(match2[3]);
+
+	const from = parseInt(match1[1]) || 0;
+	const to = parseInt(match1[2]) || total;
+
+	return (offset === from) && (end + 1 === to);
+}
+
 /**
  * Download prebuilt binary from GitHub Release.
  * If download failed, fallback to build locally.
  */
 function download() {
-	const url = `${getGithubRelease()}/${getPackageName()}`;
-	const request = https.get(url);
+	const filename = getPackageName();
+	const headers = {};
+
+	try {
+		const { size } = fs.statSync(filename);
+		headers["Range"] = `bytes=${size}-`;
+	} catch (err) {
+		if (err.code !== "ENOENT") throw err;
+	}
+
+	const url = `${getGithubRelease()}/${filename}`;
+	const request = https.get(url, { headers });
 
 	request.on("response", response => {
 		const { statusCode } = response;
+		let stream;
 
-		if (statusCode === 404) {
-			return handleInstallError(`No prebuilt binary for your environment：${url}`);
-		} else if (statusCode !== 200) {
-			return handleInstallError(`Download failed (${statusCode}): ${url}`);
+		switch (statusCode) {
+			case 404:
+				return handleInstallError(`No prebuilt binary for your environment：${url}`);
+			case 200:
+				stream = fs.createWriteStream(filename);
+				break;
+			case 206:
+				if (!checkRange(request, response)) {
+					return handleInstallError("Range  mismatch");
+				}
+				stream = fs.createWriteStream(filename, { flags: "a" });
+				break;
+			default:
+				return handleInstallError(`Download failed (${statusCode}): ${url}`);
 		}
 
-		response
-			.pipe(createBrotliDecompress())
-			.pipe(tar.extract("."));
+		const writing = response.pipe(stream).on("finish", () => {
+			clearTimeout(timer);
+
+			const extract = fs
+				.createReadStream(filename)
+				.pipe(createBrotliDecompress())
+				.pipe(tar.extract("."));
+
+			extract.on("finish", () => fs.unlinkSync(filename));
+		});
+
+		const timer = setTimeout(() => writing.destroy(), 30_000);
 	});
 
 	request.on("error", handleInstallError).end();
